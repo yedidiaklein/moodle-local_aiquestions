7de0e8bc79e4aaf82cd3677f469e06b9283128ac
Yedidia Klein <yedidia@openapp.co.il>
2023-05-11 10:26:51 +0300

    removed default from xmldb success

--- tests/createquestions_test.php
+++ tests/createquestions_test.php
@@ -39,4 +39,29 @@ class createquestions_test extends \advanced_testcase {
     public function test_dummy() {
         $this->assertTrue(false);
     }
+
+    /**
+     * Test local_aiquestions_create_questions.
+     * @covers local_aiquestions_create_questions
+     */
+    public function test_create() {
+        require_once(__DIR__ . '/../locallib.php');
+        $gift = "
+            ::Question1:: My interesting questionText
+            {
+                = right answer
+                ~ wrong1
+                ~ wrong2
+                ~ wrong3
+                ~ wrong4
+            }";
+        $question = \local_aiquestions_create_questions(2, $gift, 1, 3);
+        $this->assertEquals($question->name, 'Question1');
+        $this->assertEquals($question->questiontext, 'My interesting questionText');
+        $this->assertEquals($question->qtype, 'multichoice');
+        $this->assertEquals($question->fraction, 1);
+        $this->assertEquals($question->single, 1);
+        $this->assertEquals($question->answernumbering, 'abc');
+        $this->assertEquals($question->shuffleanswers, 1);
+    }
 }
